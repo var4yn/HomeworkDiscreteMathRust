242b258b20a02ffde9279365525f4874e947e656
var4yn <2016slavyakinnikita@gmail.com>
2024-05-03 13:58:12 +0800

    Add page5 !

--- src/tasks/Task5.jsx
+++ src/tasks/Task5.jsx
@@ -5,33 +5,64 @@ import { invoke } from "@tauri-apps/api/tauri";
 import PlayButton from "../components/PlayButton";
 import CheckAnswerButton from "../components/CheckAnswerButton";
 
-const VarButton = ( {val, onClick} ) => {
+import { OutputRender, ErrorRender } from "../utils/renders";
 
-    
+const VarButton = ( {val, index, userVals, onClick} ) => {
+    const baseStyle = "block border-2 py-1 px-1 text-center select-none cursor-pointer transition-colors ";
+    const greenStyle = baseStyle + "border-green-300 bg-green-200";
+    const redStyle = baseStyle + "border-red-400 bg-red-300";
 
 
     return (
-        <div className="block border-2 py-1 px-1 border-black text-center select-none cursor-pointer" onClick={onClick}>{val}</div>
+        <div className={ userVals[index] ? greenStyle : redStyle } onClick={onClick}>{val}</div>
     )
 }
 
+const Indicator = ( { isCheck, index, vals } ) => {        
 
+    function getStyle() {
+        if( isCheck && vals[index] !== true ) {
+            return "bg-red-500 p-1";
+        }
+        return "bg-green-500 p-1";
+    }
+
+    return (
+        <div className={`transition-colors ${getStyle()}`}>
+        </div>
+    )
+
+}
 
 function get_vars( n ) {
     let btns = [];
     for(let i = 1; i <= n; i++) {
         btns.push(`x${i}`);
     }
-
     return btns;
+}
+
+function init_user_vals( n ) {
+    let usVals = [];
+    while(n  --> 0) {
+        usVals.push(true);
+    }
+    return usVals;
+
 }
 
 // Рандомно выбрать функцию -> вычислить фиктивные переменные -> дать юзеру выбрать фиктивные переменные
 function Task5() {
 
-    const [value, setValue] = useState("-");
     const [vars, setVars] = useState(["-"]);
-    const [vals, setVals] = useState([]);
+    const [vals, setVals] = useState([true]);
+
+    const [userVals, setUserVals] = useState([true]);
+
+    const [output, setOutput] = useState("-");
+    const [err, setErr] = useState("");
+
+    const [isCheck, setIsCheck] = useState(false);
 
     const getRandom = () => {
         return Math.floor( Math.random()*3 ) + 2;
@@ -41,26 +72,44 @@ function Task5() {
         const n = getRandom();
         invoke("get_random_bool_func", { n: n + "" })
         .then(async msg => {
+            setIsCheck(false);
             setVars( get_vars(n) );
+            setUserVals( init_user_vals(n) );
             const r = await invoke("get_dummy_variable", { function: msg });
             setVals(r);
-            setValue(msg);
+            setOutput(msg);
+            setErr("");
+            //console.log(r);
         })
         .catch(err => {
-            console.log(err);
+            setErr(err);
         });
+    }
+
+    function check() {
+        setIsCheck(true);
+    }
+
+    const [f, setF] = useState(0);
+    function toggleClick( index ) {
+        if(isCheck) return;
+        userVals[index] = !userVals[index];
+        setF(!f);
     }
 
     return (
         <>
             <div className="">
                 <div className="text-4xl select-none flex justify-center">
-                    {value}
+                    { err.length > 0 ? <ErrorRender err={err}/> : <OutputRender output={output}/> }
                 </div>
                 <div className="grid mt-4 gap-3 grid-flow-col">
                     {
                         vars.map((val, i) => (
-                            <VarButton key={i} val={val}/>
+                            <div key={i} className="grid gap-2 grid-flow-row">
+                             <VarButton val={val} index={i} userVals={userVals} onClick={() => toggleClick(i)}/>
+                             <Indicator isCheck={isCheck} vals={vals} index={i}/>
+                            </div>
                         ))
                     }
                 </div>
@@ -68,7 +117,7 @@ function Task5() {
             </div>
             
             <div className="pt-8 grid gap-4 grid-flow-col">                
-                <CheckAnswerButton/>
+                <CheckAnswerButton onClick={check}/>
                 <PlayButton onClick={play}/>
             </div>
         </>
